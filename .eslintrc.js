d07be79d4d5e3e351acdbca36798653bf8a2f4ee
cibt0943 <>
2020-05-29 00:43:52 +0900

    set tsconfig and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
     es6: true,
   },
   extends: [
@@ -15,6 +14,8 @@ module.exports = {
     'plugin:prettier/recommended',
     'prettier/react',
   ],
+  globals: {
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
@@ -28,21 +29,18 @@ module.exports = {
     'prettier',
     'react',
   ],
+  root: true,
   rules: {
-    '@typescript-eslint/interface-name-prefix': [
-      'error',
-      'always',
-    ],
-    'space-before-function-paren': "off",
-    'react/prop-types': 0,
+    // 'space-before-function-paren': "off",
+    // 'react/prop-types': 0,
   },
-  settings: {
-    react: {
-      // pragma: "React",  // Pragma to use, default to "React"
-      version: "detect", // React version. "detect" automatically picks the version you have installed.
-                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                           // default to latest and warns if missing
-                           // It will default to "detect" in the future
-    },
-  },
+  // settings: {
+  //   react: {
+  //     // pragma: "React", // Pragma to use, default to "React"
+  //     version: "detect",  // React version. "detect" automatically picks the version you have installed.
+  //                         // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+  //                         // default to latest and warns if missing
+  //                         // It will default to "detect" in the future
+  //   },
+  // },
 }
