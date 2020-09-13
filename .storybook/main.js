71e2f6f81c1b1f898675c393b43148497c8ee5c0
cibt0943 <>
2020-09-13 20:53:55 +0900

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../frontend/**/*.stories.mdx",
+    "../frontend/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
+  ]
+}
